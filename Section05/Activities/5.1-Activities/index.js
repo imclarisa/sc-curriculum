0f9a33f36171a287560a6c67d51a84199124aecb
Johan Bester <johan.bester@yahoo.com>
2022-02-24 23:42:11 -0600

    Update Activities

--- Section05/Activities/5.1-Activities/index.js
+++ Section05/Activities/5.1-Activities/index.js
@@ -1,13 +1,14 @@
 const form = document.querySelector("form");
-form.addEventListener("submit", event => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log("The form was submitted!");
 
   console.log("event", event);
-  const inputs = event.target.elements;
-  // console.log("form's input elements: ", inputs);
 
-  Array.from(inputs).forEach(input => {
+  const inputs = event.target.elements;
+  console.log("form's input elements: ", inputs);
+
+  Array.from(inputs).forEach((input) => {
     console.log(
       `This input is named ${input.name} and has a value of ${input.value}`
     );
